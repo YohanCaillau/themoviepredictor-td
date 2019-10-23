4a5cd2c3c24226838e912e53709740b0a5f3e0de
Yohan Caillau <yohancaillau@live.fr>
2019-10-23 12:31:54 +0200

    scrapwikipedia

--- app.py
+++ app.py
@@ -12,22 +12,23 @@ import argparse
 import csv
 import requests
 from bs4 import BeautifulSoup
+from datetime import datetime
+import locale
+
+locale.setlocale(locale.LC_ALL, 'fr_FR')
 
 r = requests.get("https://fr.wikipedia.org/wiki/Joker_(film,_2019)")
 soup = BeautifulSoup(r.text, 'html.parser')
 
+"""
 #1
 fiche_technique = soup.find(id="Fiche_technique")
-
 #2
 parent = fiche_technique.parent 
-
 #3
 unorderedlist = parent.find_next('ul')
-
 #4
 listing = unorderedlist.findChildren('li')
-
 #5
 scrap_dict={}
 
@@ -36,13 +37,55 @@ for item in listing:
     value = item.find(['a', 'i'])
     if value:
          scrap_dict[key] = value.getText()
-
 print(scrap_dict)
 
 #listing = unorderedlist.contents[0]
 #movietitle = listing.find_all('i')[0].get_text()
 #print(movietitle)
 #movietitle = listing.contents[0].get_text()
+
+exit()"""
+
+fiche_technique = soup.find(id="Fiche_technique")
+h2_tag = fiche_technique.parent 
+ul_tag = h2_tag.find_next_sibling('ul')
+li_tags = ul_tag.find_all('li', recursive=False)
+
+for li_tag in li_tags:
+    splitted_li = li_tag.get_text().split(':')
+    data_type = splitted_li[0].strip()
+    data_value = splitted_li[1].strip()
+
+   # print(data_type)
+   # print(data_value)
+    
+    if data_type == "Titre original":
+        title = data_value
+    if data_type == "Durée":
+        duration = data_value.replace("minutes","").strip()
+    if data_type == "Dates de sortie":
+        release_dates_li_list = li_tag.find_all("li")
+        for release_date_li in release_dates_li_list:
+            release_date_splitted = release_date_li.get_text().split(':')
+            release_country = release_date_splitted[0].strip()
+            release_date_as_string =  release_date_splitted[1].strip() # 9 octobre 2019
+            if release_country == "France":
+                release_date_object = datetime.strptime(release_date_as_string, '%d %B %Y')
+                release_date_sql_string = release_date_object.strftime('%Y-%m-%d')
+    if data_type == "Classification":
+        rating_li_list = li_tag.find_all("li")
+        for rating_li in rating_li_list:
+            rating_splitted = rating_li.get_text().split(':')
+            rating_country = rating_splitted[0].strip()
+            rating_string =  rating_splitted[1].strip() # Interdit aux moins de 12 ans avec avertissement
+            if rating_country == "France":
+                if rating_string.find('12') != -1:
+                    rating = '-12'
+
+print('title =', title)
+print('duration =', duration)
+print('release_date =', release_date_sql_string)
+print('rating =', rating)          
 
 exit()
 
